1ad6b8bb18f27e992765edd4f59218e44e9397e7
jmattias1 <jmattias1@hotmail.com>
2022-06-21 19:45:10 -0300

    -

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3030;
 
-app.get("/", (req, res) => res.send("welcome Home"))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/home.html")));
+app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, "./views/babbage.html")));
 
-app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
+app.listen(port, () => console.log(`Server esta corriendo en http://localhost:` + port));
