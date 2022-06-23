806fc3bbf6478753b115be92ff403dd37817e095
jmattias1 <jmattias1@hotmail.com>
2022-06-22 22:28:37 -0300

    -

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"))
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./views/index.html")));
 app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, "./views/babbage.html")));
-app.get("/bernes-lee", (req, res) => res.sendFile(path.join(__dirname, "./views/bernes-lee.html")));
+app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname, "./views/berners-lee.html")));
 app.get("/clarke", (req, res) => res.sendFile(path.join(__dirname, "./views/clarke.html")));
 app.get("/hamilton", (req, res) => res.sendFile(path.join(__dirname, "./views/hamilton.html")));
 app.get("/hopper", (req, res) => res.sendFile(path.join(__dirname, "./views/hopper.html")));
